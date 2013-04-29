d75010571d048a5166127b4c44e4e8d8eac3b19f
Ryan Endacott <rzeg24@gmail.com>
2013-04-28 20:43:10 -0500

    added server helloworld

---
+++ helloworld/app.js
@@ -0,0 +1,20 @@
+// Import the http module that is built into Node.js
+var http = require('http');
+
+// Create a server and supply it with a function to handle requests
+http.createServer(function(request, response) {
+
+  // Write the HTTP header that tells the browser we're sending plain text
+  response.writeHead(200, {"Content-Type": "text/plain"});
+
+  // Write 'Hello world!' to the response
+  response.write('Hello world!');
+  
+  // End the response
+  response.end();
+
+}).listen(3000);
+// Make the server listen on port 3000.
+
+// State that the server is now running!
+console.log('Server listening on localhost:3000!');
