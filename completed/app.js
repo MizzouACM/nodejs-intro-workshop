91f605492b33a851c12018c3e7c6cdbaa5bfb322
Ryan Endacott <rzeg24@gmail.com>
2013-04-30 21:26:24 -0500

    Added configuration logic to a separate module

--- completed/app.js
+++ completed/app.js
@@ -1,43 +1,16 @@
-
-
 // Import the necessary modules
 var express = require('express')
   , http = require('http')
-  , path = require('path')
   , db = require('./fakedb');
 
 // Call express to create the app.
 var app = express();
 
-// Express configuration
-app.configure(function(){
-
-  // Set the port the application will run on to
-  // the environment variable PORT or 3000
-  app.set('port', process.env.PORT || 3000);
-
-  // Set the directory to get the views to be /views
-  app.set('views', __dirname + '/views');
-
-  // Use the 'hbs' view engine
-  app.set('view engine', 'hbs');
-
-  // Use the express development style logging
-  app.use(express.logger('dev'));
-
-  // Parses request input
-  app.use(express.bodyParser());
-
-  // Does the routing we'll define below!
-  app.use(app.router);
-});
-
-// When in development mode, use the error handler
-app.configure('development', function(){
-  app.use(express.errorHandler());
-});
+// Do app configuration
+require('./setup')(app);
 
 // Routes here!
+
 app.get('/', function(req, res) {
   var notes = db.getNotes();
   res.render('index', {notes: notes});
